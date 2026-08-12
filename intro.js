befb5166eafd5d8a579d562dd838ed283edbe603
Folarin Ridwanullah Abiola <folarinridwanullahabiola@gmail.com>
2026-08-12 07:46:25 +0100

    console function

--- intro.js
+++ intro.js
@@ -1,4 +1,4 @@
 console.log("I AM FOLARIN RIDWANULLAH ABIOLA")
-console.log("I am eighteen years old")
-console.log("Python is my favorite programming language")
+console.log("I am eighteen years old.")
+console.log("Python is my favorite programming language.")
 
